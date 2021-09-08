294ce759ab5bc0205e1d5573340279eb28e63524
behrozkiaei <behrozkiaei@gmail.com>
2021-09-08 14:17:21 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import axios from "axios"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
 // Routing
 import PrivateRoute from "./components/routing/PrivateRoute";
 
@@ -14,45 +10,27 @@ import RegisterScreen from "./components/screens/RegisterScreen";
 import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen";
 import ResetPasswordScreen from "./components/screens/ResetPasswordScreen";
 const App = () => {
-  axios.defaults.baseURL = 'https://tinder-ir-back.herokuapp.com/';
-  return ( <
-    Router >
-    <   
-
-
-
-    div className = "app" >
-    <
-    Switch >
-    <
-    PrivateRoute exact path = "/"
-    component = {
-      PrivateScreen
-    }
-    /> <
-    Route exact path = "/login"
-    component = {
-      LoginScreen
-    }
-    /> <
-    Route exact path = "/register"
-    component = {
-      RegisterScreen
-    }
-    /> <
-    Route exact path = "/forgotpassword"
-    component = {
-      ForgotPasswordScreen
-    }
-    /> <
-    Route exact path = "/passwordreset/:resetToken"
-    component = {
-      ResetPasswordScreen
-    }
-    /> <
-    /Switch> <
-    /div> <
-    /Router>
+  axios.defaults.baseURL = "https://tinder-ir-back.herokuapp.com/";
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <PrivateRoute exact path="/" component={PrivateScreen} />{" "}
+          <Route exact path="/login" component={LoginScreen} />{" "}
+          <Route exact path="/register" component={RegisterScreen} />{" "}
+          <Route
+            exact
+            path="/forgotpassword"
+            component={ForgotPasswordScreen}
+          />{" "}
+          <Route
+            exact
+            path="/passwordreset/:resetToken"
+            component={ResetPasswordScreen}
+          />{" "}
+        </Switch>{" "}
+      </div>{" "}
+    </Router>
   );
 };
 
