343be4f78455188deddb4f8e7e34d8fee736fdb3
behrozkiaei <behrozkiaei@gmail.com>
2021-10-15 17:05:20 +0330

    bug fixed heap

--- src/index.js
+++ src/index.js
@@ -1,43 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App.jsx';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import App from './App.jsx';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-// import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 
-// import { Provider } from "react-redux";
-// import store from "./redux/store";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#fe3c72'
-//     },
-//     secondary: {
-//       main: "#00000"
-//     },
-//     tertiary: {
-//       main: '#424242'
-//     }
-//   }
-// });
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fe3c72'
+    },
+    secondary: {
+      main: "#00000"
+    },
+    tertiary: {
+      main: '#424242'
+    }
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <MuiThemeProvider theme={theme}> */}
-      {/* <Provider store={store} > */}
-        {/* <App /> */}
-      {/* </Provider> */}
-    {/* // </MuiThemeProvider> */}
+    <MuiThemeProvider theme={theme}>
+      <Provider store={store} >
+        <App />
+      </Provider>
+    </MuiThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
